0130923f68461d1ef0bb8b62d912030094cb804d
melikarameli <melikahatunic2001@gmail.com>
2022-04-06 14:20:33 +0200

    Added ex01

---
+++ ex01/src/index.js
@@ -0,0 +1,13 @@
+// Only change code below this line
+var myPet = {
+    species: "Husky",
+    name: "Ghost",
+    legs: 4,
+    firends: ["Shadow", "Jack"]
+};
+function myFunction(myObj) {
+    return myObj;
+}
+// Only change code above this line
+console.log(myFunction(myPet)); // Change this line
+module.exports = { myPet, myFunction };
