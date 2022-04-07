e414f0d78ad1f27f6ce7eda2dadca95a71f3d997
melikarameli <melikahatunic2001@gmail.com>
2022-04-07 09:30:17 +0200

    Added ex09

--- ex09/src/index.js
+++ ex09/src/index.js
@@ -1,15 +1,19 @@
+var myObj = {
+    title: "Titanic",
+    song: "My Heart Will Go On",
+    genre: "drama"
+    
+};
 function myFunction(myObj, checkProp) {
     // Only change code below this line
-    var myObj = {
-        title: "Titanic",
-        song: "My Heart Will Go On",
-        genre: "drama"
+    if (myObj.hasOwnProperty(checkProp)) {
+        return myObj[checkProp];
     }
-    myObj.hasOwnProperty("title");
-    myObj.hasOwnProperty("song");
-    myObj.hasOwnProperty("genre");
-    myObj.hasOwnProperty("actor");
-    return "Change Me!";
+    return "Not found";
     // Only change code above this line
 }
+console.log(myFunction(myObj, "title"));
+console.log(myFunction(myObj, "song"));
+console.log(myFunction(myObj, "genre"));
+console.log(myFunction(myObj, "actor"));
 module.exports = myFunction;
